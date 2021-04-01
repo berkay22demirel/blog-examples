5c096970357a080a2bc052e3ffc538c5020429ec
berkay-demirel <berkay_demirel22@hotmail.com>
2021-04-01 21:01:31 +0300

    Initial commit

---
+++ elastic-search-basic-example/src/main/java/com/berkay22demirel/elasticsearchbasicexample/ElasticSearchBasicExampleApplication.java
@@ -0,0 +1,13 @@
+package com.berkay22demirel.elasticsearchbasicexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchBasicExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchBasicExampleApplication.class, args);
+	}
+
+}
---
+++ elastic-search-basic-example/src/test/java/com/berkay22demirel/elasticsearchbasicexample/ElasticSearchBasicExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkay22demirel.elasticsearchbasicexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchBasicExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
