48fb9636fa0621deceee49ef07f7a260ff002b3c
berkay-demirel <berkay_demirel22@hotmail.com>
2021-05-07 23:58:54 +0300

    init rabbitmq example

--- elastic-search-basic-example/src/main/java/com/berkay22demirel/elasticsearchbasicexample/service/BookService.java
+++ elastic-search-basic-example/src/main/java/com/berkay22demirel/elasticsearchbasicexample/service/BookService.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.util.Calendar;
 import java.util.List;
 
 @Service
@@ -17,12 +16,12 @@ public class BookService {
 
     @PostConstruct
     public void init() {
-        Book book1 = new Book("B00000001","Lord of the Rings", "J. R. R. Tolkien", Calendar.getInstance().getTime());
-        Book book2 = new Book("B00000002","Hobbit", "J. R. R. Tolkien", Calendar.getInstance().getTime());
-        Book book3 = new Book("B00000003","Game of Thrones", "George R. R. Martin", Calendar.getInstance().getTime());
-        bookRepository.save(book1);
-        bookRepository.save(book2);
-        bookRepository.save(book3);
+        //Book book1 = new Book("B00000001","Lord of the Rings", "J. R. R. Tolkien", Calendar.getInstance().getTime());
+        //Book book2 = new Book("B00000002","Hobbit", "J. R. R. Tolkien", Calendar.getInstance().getTime());
+        //Book book3 = new Book("B00000003","Game of Thrones", "George R. R. Martin", Calendar.getInstance().getTime());
+        //bookRepository.save(book1);
+        //bookRepository.save(book2);
+        //bookRepository.save(book3);
     }
 
     public List<Book> search(String search) {
