c82b661dd665272067df2c5d162cd18c63c3485e
berkay-demirel <berkay_demirel22@hotmail.com>
2022-04-09 11:39:10 +0300

    add generics example for effective java

--- effective-java/src/main/java/generics/GenericsExample2.java
+++ effective-java/src/main/java/generics/GenericsExample2.java
@@ -6,7 +6,7 @@ public class GenericsExample2 {
         add(5, 5.2);
     }
 
-    private static <T, E extends Number> void add(E param1, E param2) {
+    private static <E extends Number> void add(E param1, E param2) {
         Number a = param1.doubleValue() + param2.doubleValue();
         System.out.println(a);      //10.2
     }
